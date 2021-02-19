bbdd84d786e375f96c5133ca2aeab71e22edc4e4
Sandy Smith <toosandra@gmail.com>
2021-02-18 21:58:05 -0500

    creaed projects list in places file

---
+++ src/static/places.js
@@ -0,0 +1,65 @@
+const places = [
+    // {
+    //     title: 'C-19 Tracker',
+    //     description:
+    //     "Using React and MongoDB, the user can check on Covid-19 Status and protocols of any State.",
+    //     imageUrl: '/assets/covid19.png',
+    //     demoSrc: 'www.slsmi285.github.io/c19-tracker',
+    //     gitSrc: 'www.github.com/slsmi285/c19-tracker',
+        
+        
+    // },
+    {
+        title: 'Cosmic Cocktails',
+        description:
+        "Search a cocktail recipe by its name, or search for something new by entering a letter or ingredient.",
+        imageUrl: '/assets/drinkcover.jpg',
+        demoSrc: 'https://cryptic-tundra-79116.herokuapp.com/',
+        gitSrc: 'https://github.com/slsmi285/Project2_CosmicCocktails',
+        
+         
+    },
+    // {
+    //     title: 'React Tic Tac Toe',
+    //     description:
+    //     "Using React and Material-UI. Play the game! Tic Tac Toe....",
+    //     imageUrl: '/assets/web.jpg',
+    //     demoSrc: '',
+    //     gitSrc: '',
+        
+         
+    // },
+    {
+        title: 'Train Station Scheduler',
+        description:
+        "Enter the state you are traveling to to find out covid-19 status and state info.",
+        imageUrl: '/assets/train.jpg',
+        demoSrc: 'https://slsmi285.github.io/Train-Station-Scheduler/',
+        gitSrc: 'https://github.com/slsmi285/Train-Station-Scheduler',
+        
+        
+        
+    },
+    {
+        title: 'Scraper News',
+        description:
+        "Search a cocktail recipe by its name, or search for something new by entering a letter or ingredient.",
+        imageUrl: '/assets/web.jpg',
+        demoSrc: 'https://vast-spire-84282.herokuapp.com/',
+        gitSrc: 'https://github.com/slsmi285/scraper_news',
+        
+         
+    },
+    // {
+    //     title: 'Liri Node',
+    //     description:
+    //     "Using Nodejs CLI, all your answers will show up in the terminal.",
+    //     imageUrl: '/assets/liri.jpg',
+    //     demoSrc: 'https://slsmi285.github.io/liri-node-app/',
+    //     gitSrc: 'https://github.com/slsmi285/liri-node-app',
+        
+        
+    // },
+
+];
+export default places;
