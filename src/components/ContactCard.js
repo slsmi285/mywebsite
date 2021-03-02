37528683327211d9488b807bb4b7c6b6c65c1bde
Sandy Smith <toosandra@gmail.com>
2021-03-01 19:57:09 -0500

    update links

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -5,6 +5,7 @@ import CardActions from '@material-ui/core/CardActions';
 import { makeStyles } from '@material-ui/core/styles';
 import { Card, Button, Box, Grid } from '@material-ui/core';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 
 
@@ -39,6 +40,14 @@ export default function ContactCard() {
               color="primary"
               href={`https://www.linkedin.com/in/sandrasmithdev1548`} target='_blank'>
               <LinkedInIcon />
+            </Button>
+            <Button 
+              style={{ display: 'flex' }}
+              fullWidth="true"
+              size="medium"
+              color="primary"
+              href={`https://github.com/slsmi285`} target='_blank'>
+              <GitHubIcon />
             </Button>
           </CardActions>
       </Box>
