2a3e72cc764848989069514aca01219692a8863e
Sandy Smith <toosandra@gmail.com>
2021-03-07 10:41:07 -0500

    solving background issues in build

--- src/App.js
+++ src/App.js
@@ -12,24 +12,25 @@ import dahlia3 from './assets/dahlia3.jpg';
 
 
 
-const useStyles = makeStyles({
-  root: {
-    minHeight: '100vh',
-    backgroundImage: `url(${dahlia3})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-  },
+// const useStyles = makeStyles({
+//   root: {
+//     minHeight: '100vh',
+//     backgroundImage: `url(${dahlia3})`,
+//     backgroundRepeat: 'no-repeat',
+//     backgroundSize: 'cover',
+//   },
 
 
 
-});
+// });
 
 
 function App() {
-  const classes = useStyles();
+  // const classes = useStyles();
 
   return (
-    <div className={classes.root}>
+    // <div className={classes.root}>
+    <div style={{ backgroundImage: `url(${dahlia3})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', minHeight: '100vh' }}>
 
       <CssBaseline />
       <div style={{ position: 'absolute' }}>
