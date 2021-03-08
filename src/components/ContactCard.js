d8cf022af75872206b53d188d8c9f3c4d76401e7
Sandy Smith <toosandra@gmail.com>
2021-03-08 01:24:38 -0500

    updating coding

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -21,11 +21,11 @@ const useStyles = makeStyles((theme) => ({
   ctnForm: {
     // minHeight: '60vh',
     marginLeft: '30px',
-    backgroundColor: 'brown',
+
 
   },
   infoBox: {
-    backgroundColor: '#00000000',
+    
     marginLeft: '30px',
     display: 'center',
   },
@@ -48,8 +48,8 @@ export default function ContactCard() {
       <Box className={classes.infoBox}>
         <Card component="form" className={classes.cntForm} style={{ backgroundColor: 'black', border: '4px double white' }}>
           <h3 style={{ color: "limegreen", display: "center", marginLeft: "10px" }} variant="outlined" size="large" fullWidth="true">CONTACT INFO</h3> 
-          <h5 style={{ color: "white" }}>Email: sls2code@gmail.com </h5>
-          <h5 style={{ color: "white" }}>Phone: (865) 696-1106</h5>
+          <h5 style={{ color: "limegreen" }}>Email: sls2code@gmail.com </h5>
+          <h5 style={{ color: "limegreen" }}>Phone: (865) 696-1106</h5>
       {/* <ContactForm /> */}
         </Card>
           <CardActions>
